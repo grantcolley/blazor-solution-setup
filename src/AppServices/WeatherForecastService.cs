0f5c6985a72996ce7a810b3df1901f3e26cf3184
grantcolley <grantcolley@yahoo.com>
2021-02-21 17:10:28 +0000

    Create AppServices.csproj
    Create AppServices.csproj

---
+++ src/AppServices/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+﻿using AppCore.Interface;
+using AppCore.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppServices
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>
+                (await _httpClient.GetStreamAsync($"WeatherForecast"), new JsonSerializerOptions(JsonSerializerDefaults.Web));
+        }
+    }
+}
