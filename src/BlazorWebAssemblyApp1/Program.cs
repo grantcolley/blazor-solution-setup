fcbf31d14a748e94ea3a8c12874bcf324575205e
grantcolley <grantcolley@yahoo.com>
2021-02-24 20:46:44 +0000

    Add new BlazorWebAssemblyApp project
    Add new BlazorWebAssemblyApp project

---
+++ src/BlazorWebAssemblyApp1/Program.cs
@@ -0,0 +1,25 @@
+using AppCore.Interface;
+using AppServices;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<BlazorShared.App>("app");
+
+            builder.Services.AddHttpClient<IWeatherForecastService, WeatherForecastService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44303");
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
