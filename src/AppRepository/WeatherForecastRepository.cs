85b58efdaabc4c88795bbd799de22fcdb676f016
grantcolley <grantcolley@yahoo.com>
2021-03-13 07:56:09 +0000

    Update WeatherForecastRepository
    Update WeatherForecastRepository

--- src/AppRepository/WeatherForecastRepository.cs
+++ src/AppRepository/WeatherForecastRepository.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using AppCore.Interface;
+using AppCore.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppRepository
 {
-    class WeatherForecastRepository
+    public class WeatherForecastRepository : IWeatherForecastRepository
     {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> GetWeatherForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
