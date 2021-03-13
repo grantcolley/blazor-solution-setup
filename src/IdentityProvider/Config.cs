6d035dc7e0caf2936a07c49a95990f6fc937f306
grantcolley <grantcolley@yahoo.com>
2021-03-13 11:04:51 +0000

    Add the IdentityProvider project
    Add the IdentityProvider project

---
+++ src/IdentityProvider/Config.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityProvider
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("weatherapiread")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("weatherapi", "The Weather API")
+                {
+                    Scopes = new [] { "weatherapiread" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "blazorwebassemblyapp",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = { "https://localhost:44500" },
+                    AllowedScopes = { "openid", "profile", "weatherapiread" },
+                    RedirectUris = { "https://localhost:44500/authentication/login-callback" },
+                    PostLogoutRedirectUris = { "https://localhost:44500/" },
+                    Enabled = true
+                },
+
+                new Client
+                {
+                    ClientId = "blazorserverapp",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets = { new Secret("blazorserverappsecret".Sha256()) },
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = { "https://localhost:44600" },
+                    AllowedScopes = { "openid", "profile", "weatherapiread" },
+                    RedirectUris = { "https://localhost:44600/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44600/signout-oidc" },
+                },
+            };
+    }
+}
