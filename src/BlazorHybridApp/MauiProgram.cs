7f8be8374d960a8ce563dac38a6db62800ecc168
grantcolley <grantcolley@yahoo.com>
2022-10-29 10:35:39 +0100

    Update MauiProgram.cs

--- src/BlazorHybridApp/MauiProgram.cs
+++ src/BlazorHybridApp/MauiProgram.cs
@@ -1,8 +1,10 @@
 ﻿using BlazorHybridApp.Authentication;
 using BlazorHybridApp.HttpDev;
+using Core.Interface;
 using Core.Model;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebView.Maui;
+using Services;
 
 namespace BlazorHybridApp
 {
@@ -41,7 +43,7 @@ namespace BlazorHybridApp
                 identityAuthenticationStateProviderOptions.ClientId = "blazorhybridapp";
                 identityAuthenticationStateProviderOptions.AdditionalProviderParameters.Add("audience", "https://WebApi.com");
                 identityAuthenticationStateProviderOptions.Scope = "openid profile weatherapiread";
-                identityAuthenticationStateProviderOptions.RoleClaim = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+                identityAuthenticationStateProviderOptions.RoleClaim = "role";
                 identityAuthenticationStateProviderOptions.RedirectUri = "myapp://callback";
                 identityAuthenticationStateProviderOptions.PostLogoutRedirectUris = "myapp://callback";
                 //identityAuthenticationStateProviderOptions.RedirectUri = "http://localhost/callback"; // https://github.com/dotnet/maui/issues/8382
@@ -61,6 +63,14 @@ namespace BlazorHybridApp
                 client.BaseAddress = new Uri("https://localhost:44320");
             });
 #endif
+
+            builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>(sp =>
+            {
+                var tokenProvider = sp.GetRequiredService<TokenProvider>();
+                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                var httpClient = httpClientFactory.CreateClient("webapi");
+                return new WeatherForecastService(httpClient, tokenProvider);
+            });
 
             return builder.Build();
         }
